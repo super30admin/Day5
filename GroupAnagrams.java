909ef04ec48dc993795fc6ff3a4fdcf1317302ab
Sahil Raut <sahilraut@Sahils-MacBook-Air.local>
2024-05-15 13:03:02 -0700

    Done

---
+++ GroupAnagrams.java
@@ -0,0 +1,23 @@
+// Your code here along with comments explaining your approach
+// We sort each word in the input array and store it in a map with the sorted word as key and the original word as value.
+// We return the values of the map as the output.
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
