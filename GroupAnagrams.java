8e3070c6b8a08dfedd24e11e5afe88203c0307fe
Yamini <yamini.bommineni1@t-mobile.com>
2024-06-29 16:27:01 -0700

    Completed Hashing1

---
+++ GroupAnagrams.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<Double, List<String>> result = new HashMap<>();
+        int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                101, 103 };
+        for (String s : strs) {
+            Double key = (double)(1);
+            for (char c : s.toCharArray()) {
+                key = key * primes[c - 'a'];
+            }
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList());
+            }
+            result.get(key).add(s);
+        }
+        return new ArrayList(result.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams obj = new GroupAnagrams();
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(obj.groupAnagrams(input));
+    }
+}
