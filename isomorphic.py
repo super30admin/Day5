24eb7d4f854e32e6d78c029787fedf3d3aa6f1f1
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-06-30 21:13:19 -0400

    Isomorphic Updated

--- isomorphic.py
+++ isomorphic.py
@@ -1,17 +1,32 @@
+
+
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        slen = len(s)
+        tlen = len(t)
 
-        smap = {}
-        
-        for string in strs:
-            #liststr = list(string)
-            sortstr = sorted(string)
-            sortstr = ''.join(sortstr)
-            if sortstr in smap:
-                smap[sortstr].append(string)
-                
+        if slen != tlen:
+            return True
+
+        sMap = { }
+        tMap = { }
+
+        for i in range(slen):
+            sChar = s[i]
+            tChar = t[i]
+
+            if sChar in sMap:
+                if sMap.get(sChar) != tChar:
+                    return False
             else:
-                smap[sortstr] = [string]
-                
+                sMap[sChar] = tChar
 
-        return smap.values()
+
+            if tChar in tMap:
+                if tMap.get(tChar) != sChar:
+                    return False
+            else:
+                tMap[tChar] = sChar
+
+        return True
+        
