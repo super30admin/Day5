8abe365393c76baa2901ee2c48de2777e3ddd24e
Manasi Burange <burangemanasi@gmail.com>
2024-10-29 13:28:14 -0700

    Completed Hashing-1 Problems

--- 2024_Problem14_Grouping_Anagrams.java
+++ 2024_Problem14_Grouping_Anagrams.java
@@ -4,7 +4,32 @@
 //Space Complexity: O(1)
 //Prime Product Solution
 
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<Double, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            double key = calculatePrimeProduct(str);
+            // Add the string to the correct list in the map
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
 
+    private double calculatePrimeProduct(String str) {
+        int[] primes = {
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
+        };
+        double product = 1;
+        for (char c : str.toCharArray()) {
+            product *= primes[c - 'a'];
+        }
+        return product;
+    }
+}
 
 
 //Time Complexity: O(n) * (klog(k) + O(k))
