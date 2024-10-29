e2c2d8f73b189829a40b88844cfd5d16e066f4df
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-10-28 19:42:52 -0700

    2/3 Done

--- isomorphicStrings.py
+++ isomorphicStrings.py
@@ -1,32 +1,31 @@
+# We will create 2 maps for s:t and t:s. We will zip all elements together so it is easy to unpack them.
+#First we will check the 2 flase conditions / value x not present in Map 1 ;but key present t:s -> end loop False
+#False condition 2 is if Map 1 value is not equal to y
+#otherwise add add map[y] as x and map[x] as y.
+# Time Complexity :O(n) for iteration zip
+# Space complexity :O(m) for s and t being used together
+
 class Solution:
-    def isIsomorphic(s: str, t: str) -> bool:
-        sMap = [] #use this for s:t
-        tMap = [] #use this for t:s
-
-        if len(s)==[] and len(t)==[]:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # base conditions
+        if len(s)=={} and len(t)=={}:
             return True
-        if len(s)==[] and len(t)!=[] or (len(s)!=[] and len(t)==[]):
+        if (len(s)=={} and len(t)!={}) or (len(s)!={} and len(t)=={}):
             return False
-        if len(s)!=len(t):
+        if  len(s) != len(t):
             return False
-        
-        
-        for i in range(0,len(s)-1):
-            sChar = s[i]
-            tChar = t[i]
-            
-            if (sMap[sChar-"a"] != 0):
-               if (sMap[sChar-"a"] != tChar):
-                   return False
-               else:
-                   sMap[sChar - "a"] = tChar 
-
-            if (tMap[tChar-"a"] != 0):
-               if (tMap[tChar-"a"] != sChar):
-                   return False
-               else:
-                   tMap[tChar - "a"] = sChar
-
-x = Solution.isIsomorphic(s = "hello",t = "world")
 
 
+        sMap = {} #use this for s:t
+        tMap = {} #use this for t:s
+
+        for x,y in zip(s,t):        # creates (h:j),(e:e),(l:n),(o:y) pairs
+            if x not in sMap:       # if x not in m1 [,,,] False condition 1
+                if y in tMap:       # but y in m2 [,,,]
+                    return False    # Dead End...otherwise
+                sMap[x] = y         # smap -> t
+                tMap[y] = x         # tmap -> s
+            elif sMap[x] != y:      # otherwise False condition 2
+                return False
+        return True
+         
--- prime.py
+++ prime.py
@@ -17,4 +17,4 @@ def generate_primes(n):
 # Example usage:
 n = 26
 result = generate_primes(n)
-print(result)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+print(result)  # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
