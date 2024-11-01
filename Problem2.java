1560222eecb162bff294d7f8020050bb75552532
kurian <openfeather@hotmail.com>
2024-11-01 10:57:33 -0400

    Hashing1  solved

--- Problem2.java
+++ Problem2.java
@@ -1,2 +1,35 @@
-public class Problem1 {
+import java.util.HashMap;
+
+//https://leetcode.com/problems/isomorphic-strings/
+//Time complexity O(N)
+//Space complexity O(1) as space could not be more than the character set
+public class Problem2 {
+    public boolean isIsomorphic(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character,Character> st1 = new HashMap<>();
+        HashMap<Character,Character> st2 = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if(!st1.containsKey(sc)){
+                st1.put(sc,tc);
+            }else{
+                if(!st1.get(sc).equals(tc)){
+                    return false;
+                }
+            }
+            if(!st2.containsKey(tc)){
+                st2.put(tc,sc);
+            }else{
+                if(!st2.get(tc).equals(sc)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- Problem3.java
+++ Problem3.java
@@ -1,2 +1,42 @@
-public class Problem1 {
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/word-pattern/description/
+//Time complexity O(N)
+//Space complexity O(N)
+public class Problem3 {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character,String> map = new HashMap<>();
+        Map<String,Character> strMap = new HashMap<>();
+        char[] patternChar = pattern.toCharArray();
+        String[] strArr = s.split(" ");
+        if(patternChar.length !=strArr.length ){
+            return false;
+        }
+        for(int i=0;i<patternChar.length;i++){
+            if(map.containsKey(patternChar[i])){
+                if(!map.get(patternChar[i]).equals(strArr[i])){
+                    return false;
+                }
+            }else{
+                map.put(patternChar[i],strArr[i]);
+
+
+            }
+
+
+            if(strMap.containsKey(strArr[i])){
+                if(!strMap.get(strArr[i]).equals(patternChar[i])){
+                    return false;
+                }
+            }else{
+                strMap.put(strArr[i],patternChar[i]);
+
+
+            }
+
+        }
+        return true;
+
+    }
 }
